654ca3b798803bc5dca60c150529b9520f004ce4
Paulin Lambert <paulin.lambert@gmail.com>
2021-02-10 11:41:11 +0100

    FIX scheduler was not working

--- sensor/logger.py
+++ sensor/logger.py
@@ -8,12 +8,14 @@ API_URL = os.environ.get('API_URL')
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
+SECOND_BETWEEN_MEASURE = 20
+
 s = sched.scheduler(time.time, time.sleep)
 
 def measure(sc):
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     print(humidity, temperature)
-    s.enter(60, 1, do_something, (sc,))
+    s.enter(SECOND_BETWEEN_MEASURE, 1, measure, (sc,))
 
-s.enter(60, 1, do_something, (s,))
+s.enter(SECOND_BETWEEN_MEASURE, 1, measure, (s,))
 s.run()
