55f5ca9e808da8b24d7a58472cd5978b23b8efbc
Paulin Lambert <paulin.lambert@gmail.com>
2021-02-10 10:52:14 +0100

    NEW implement periodical measures

--- sensor/logger.py
+++ sensor/logger.py
@@ -1,8 +1,19 @@
 import Adafruit_DHT 
+import requests
+import os
+import sched, time
+
+API_URL = os.environ.get('API_URL')
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-while True:
+s = sched.scheduler(time.time, time.sleep)
+
+def measure(sc):
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     print(humidity, temperature)
+    s.enter(60, 1, do_something, (sc,))
+
+s.enter(60, 1, do_something, (s,))
+s.run()
